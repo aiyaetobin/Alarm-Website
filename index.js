92455073ea60abe01cea47093e4c823704088116
Aiya <aiyaetobin@gmail.com>
2026-08-11 18:04:50 -0700

    added signup endpoint

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const pool = require('./src/db');
 
-const express = require('express'); //uses express library
-const app = express(); //creates a new express application
+const app = express();
 
-app.use(express.static('public')); // server files from public when someone visits the site
+app.use(express.json());
+app.use(express.static('public'));
 
-app.listen(3000, () => { //starts the server and listens for requests
-    console.log('Server is running on port 3000'); //logs a message to the console when the server starts
-})
+app.post('/signup', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log('Signup attempt:', username);
+  res.json({ message: 'got your data' });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
