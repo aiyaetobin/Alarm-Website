8c5e927179462359833425ad26c5b174b0c4a48b
Aiya <aiyaetobin@gmail.com>
2026-08-22 23:22:37 -0700

    update

--- src/db.js
+++ src/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME || 'alarm_challenge'
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
