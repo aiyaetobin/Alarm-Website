679d724aae3f1f732bfd0c97a9f66812fa1a46a1
Aiya <aiyaetobin@gmail.com>
2026-08-26 21:40:18 -0700

    Update index2.js

--- index2.js
+++ index2.js
@@ -10,8 +10,7 @@ const sessions = {};
 app.use(express.static('public'));
 app.use(express.json());
 //custom middleware
-app.use((req, res, next) =>
-{ //session is added to the session object later
+app.use((req, res, next) => { //session is added to the session object later
     //
     const sessionId = req.cookies.userId;
     if (sesssionId && sessions[sessionId])
@@ -22,18 +21,41 @@ app.use((req, res, next) =>
 });
 
 //login 
-app.post('/login', (req, res) =>
-{
+app.post('/login', async (req, res) => {
+    const password = req.body.password;
+    const username = req.body.username;
+try {
+     const result = await pool.query(
+        'SELECT id, password FROM users WHERE username = $1',
+        [username]
+    );
+    const user = result.rows(0);
+    
+    if(result.rows.length() == 0){
+        return res.status(401).json({message: 'account not found sign up'});
+    }
+    
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if(passwordMatch){
+        const sessionNum = 
+        sessions[user.id] = 
+    }
+
+}catch (error) {
+    res.status(400);
+}
+
+
+
 
 })
 
 //signup 
-app.post('/signup', async (req, res) =>
-{
+app.post('/signup', async (req, res) => {
     const password = req.body.password;
     const username = req.body.username;
-    try
-    {
+    try {
         const hashedPassword = bcrypt.hashSync(password, 10);
 
         const result = await pool.query(
@@ -41,11 +63,10 @@ app.post('/signup', async (req, res) =>
             [username, password]
         );
 
-        res.json(message: 'sign up successful', userId: result.rows[0].id);
+        res.json({message: 'sign up successful', userId: result.rows[0].id});
 
-    } catch (error)
-    {
-        res.status(400).json(error: error.message);
+    } catch (error) {
+        res.status(400).json({error: error.message});
     }
 
 
