e44c5cdd5612fc2583a5b428936674358d25cb94
Aiya <aiyaetobin@gmail.com>
2026-08-28 17:38:38 -0700

    l

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const stripe = require('stripe', process.env.STRIPE_SECRET_KEY);
-const pool = require('src/dv');
+const pool = require('./src/db');
 const cookieParser = require('cookie-parser');
 const bcryptjs = require('bcryptjs');
 
@@ -9,11 +9,14 @@ const sessions = {};
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 //custom middleware
-app.use((req, res, next) => { //session is added to the session object later
+app.use((req, res, next) =>
+{ //session is added to the session object later
     //
-    const sessionId = req.cookies.userId;
-    if (sesssionId && sessions[sessionId])
+    const sessionId = req.cookies.sessionId;
+
+    if (sessionId && sessions[sessionId])
     {
         req.userId = sessions[sessionId].userId;
     }
@@ -21,54 +24,63 @@ app.use((req, res, next) => { //session is added to the session object later
 });
 
 //login 
-app.post('/login', async (req, res) => {
+app.post('/login', async (req, res) =>
+{
     const password = req.body.password;
     const username = req.body.username;
-try {
-     const result = await pool.query(
-        'SELECT id, password FROM users WHERE username = $1',
-        [username]
-    );
-    const user = result.rows(0);
-    
-    if(result.rows.length() == 0){
-        return res.status(401).json({message: 'account not found sign up'});
+    try
+    {
+        const result = await pool.query(
+            'SELECT id, password FROM users WHERE username = $1',
+            [username]
+        );
+        const user = result.rows(0);
+
+        if (result.rows.length() == 0)
+        {
+            return res.status(401).json({ message: 'account not found sign up' });
+        }
+
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+
+        if (passwordMatch)
+        {
+            const sessionId = crypto.randomUUID();
+            sessions[user.id] = { userId: user.id };
+
+            res.cookies('sessionId', sessionId, { httpOnly: true });
+            res.json({ message: 'Login successful', userId: user.id });
+        } else
+        {
+            res.status(401).json({ error: 'Wrong password' });
+        }
+
+    } catch (error)
+    {
+        res.status(400).json({ error: error.message });
     }
-    
-    const passwordMatch = bcrypt.compareSync(password, user.password);
-
-    if(passwordMatch){
-        const sessionId = crypto.randomUUID();
-        sessions[user.id] = {userId: user.id};
-        
-        res.cookies(userId, user.id
-            
-        )
-
-    }
-
-}catch (error) {
-    res.status(400);
-}
 
 });
 
 //signup 
-app.post('/signup', async (req, res) => {
+app.post('/signup', async (req, res) =>
+{
     const password = req.body.password;
     const username = req.body.username;
-    try {
+    try
+    {
         const hashedPassword = bcrypt.hashSync(password, 10);
 
         const result = await pool.query(
             'INSERT INTO users (username, password) VALUES($1, $2) RETURNING id',
-            [username, password]
+            [username, hashedPassword]
         );
 
-        res.json({message: 'sign up successful', userId: result.rows[0].id});
+        res.json({ message: 'sign up successful', userId: result.rows[0].id });
 
-    } catch (error) {
-        res.status(400).json({error: error.message});
+    } catch (error)
+    {
+        res.status(400).json({ error: error.message });
     }
 
 
